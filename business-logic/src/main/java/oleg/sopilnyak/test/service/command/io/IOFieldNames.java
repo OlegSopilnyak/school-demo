1ef9cca5c7040e6e53db3cf42b44f56e3820fba2
Oleg Sopilnyak <olegsopilnyak@bigmir.net>
2024-12-04 20:46:23 +0100

    Output results subsystem made (field-names)

---
+++ business-logic/src/main/java/oleg/sopilnyak/test/service/command/io/IOFieldNames.java
@@ -0,0 +1,10 @@
+package oleg.sopilnyak.test.service.command.io;
+
+/**
+ * The names allowed in JSON
+ */
+public interface IOFieldNames {
+    String VALUE_FIELD_NAME = "value";
+    String TYPE_FIELD_NAME = "type";
+    String NESTED_TYPE_FIELD_NAME = "nested-type";
+}
