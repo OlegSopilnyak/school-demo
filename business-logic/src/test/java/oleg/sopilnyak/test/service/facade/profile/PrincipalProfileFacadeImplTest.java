0d6e9db1e01cb6151a3737460420e362d6eacb17
Oleh Sopilniak <oleh.sopilniak@globallogic.com>
2025-11-09 10:56:42 +0200

    person profile facade fix unit tests

--- business-logic/src/test/java/oleg/sopilnyak/test/service/facade/profile/PrincipalProfileFacadeImplTest.java
+++ business-logic/src/test/java/oleg/sopilnyak/test/service/facade/profile/PrincipalProfileFacadeImplTest.java
@@ -1,9 +1,21 @@
 package oleg.sopilnyak.test.service.facade.profile;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import oleg.sopilnyak.test.school.common.exception.profile.ProfileNotFoundException;
+import oleg.sopilnyak.test.school.common.model.PersonProfile;
 import oleg.sopilnyak.test.school.common.model.PrincipalProfile;
 import oleg.sopilnyak.test.school.common.model.StudentProfile;
-import oleg.sopilnyak.test.school.common.model.PersonProfile;
 import oleg.sopilnyak.test.school.common.persistence.profile.ProfilePersistenceFacade;
 import oleg.sopilnyak.test.service.command.executable.profile.principal.CreateOrUpdatePrincipalProfileCommand;
 import oleg.sopilnyak.test.service.command.executable.profile.principal.DeletePrincipalProfileCommand;
@@ -17,41 +29,33 @@ import oleg.sopilnyak.test.service.facade.profile.impl.PrincipalProfileFacadeImp
 import oleg.sopilnyak.test.service.mapper.BusinessMessagePayloadMapper;
 import oleg.sopilnyak.test.service.message.payload.PrincipalProfilePayload;
 
+import java.util.Optional;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import java.util.Optional;
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PrincipalProfileFacadeImplTest {
     private static final String PROFILE_FIND_BY_ID = "profile.principal.findById";
     private static final String PROFILE_CREATE_OR_UPDATE = "profile.principal.createOrUpdate";
     private static final String PROFILE_DELETE = "profile.principal.deleteById";
-    ProfilePersistenceFacade persistence = mock(ProfilePersistenceFacade.class);
-    BusinessMessagePayloadMapper payloadMapper = mock(BusinessMessagePayloadMapper.class);
-    @Spy
-    CommandsFactory<PrincipalProfileCommand<?>> factory = buildFactory();
+
+    @Mock
+    ProfilePersistenceFacade persistence;
     @Mock
     ApplicationContext applicationContext;
     FindPrincipalProfileCommand findCommand;
     CreateOrUpdatePrincipalProfileCommand updateCommand;
     DeletePrincipalProfileCommand deleteCommand;
-
-    @Spy
-    @InjectMocks
+    CommandsFactory<PrincipalProfileCommand<?>> factory;
+    BusinessMessagePayloadMapper payloadMapper = mock(BusinessMessagePayloadMapper.class);
     PrincipalProfileFacadeImpl facade;
+
     @Mock
     PrincipalProfile profile;
     @Mock
@@ -59,15 +63,17 @@ class PrincipalProfileFacadeImplTest {
 
     @BeforeEach
     void setUp() {
-        ReflectionTestUtils.setField(updateCommand, "applicationContext", applicationContext);
+        factory = spy(buildFactory());
+        facade = spy(new PrincipalProfileFacadeImpl(factory, payloadMapper));
     }
 
     @Test
     void shouldFindProfileById() {
+        doReturn(findCommand).when(applicationContext).getBean("profilePrincipalFind", PrincipalProfileCommand.class);
         Long id = 600L;
-        when(payloadMapper.toPayload(any(PersonProfile.class))).thenReturn(payload);
+        doReturn(payload).when(payloadMapper).toPayload(any(PersonProfile.class));
         doCallRealMethod().when(persistence).findPrincipalProfileById(id);
-        when(persistence.findProfileById(id)).thenReturn(Optional.of(profile));
+        doReturn(Optional.of(profile)).when(persistence).findProfileById(id);
 
         Optional<PrincipalProfile> result = facade.findPrincipalProfileById(id);
 
@@ -82,6 +88,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldNotFindProfileById() {
+        doReturn(findCommand).when(applicationContext).getBean("profilePrincipalFind", PrincipalProfileCommand.class);
         Long id = 610L;
 
         Optional<PrincipalProfile> faculty = facade.findPrincipalProfileById(id);
@@ -96,6 +103,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldNotFindProfileById_WrongProfileType() {
+        doReturn(findCommand).when(applicationContext).getBean("profilePrincipalFind", PrincipalProfileCommand.class);
         Long id = 611L;
         doCallRealMethod().when(persistence).findPrincipalProfileById(id);
         when(persistence.findProfileById(id)).thenReturn(Optional.of(mock(StudentProfile.class)));
@@ -145,6 +153,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldDeleteProfileById() {
+        doReturn(deleteCommand).when(applicationContext).getBean("profilePrincipalDelete", PrincipalProfileCommand.class);
         Long id = 602L;
         when(persistence.findPrincipalProfileById(id)).thenReturn(Optional.of(profile));
         when(persistence.toEntity(profile)).thenReturn(profile);
@@ -162,6 +171,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldNotDeleteProfile_ProfileNotExists() throws ProfileNotFoundException {
+        doReturn(deleteCommand).when(applicationContext).getBean("profilePrincipalDelete", PrincipalProfileCommand.class);
         Long id = 615L;
 
         ProfileNotFoundException exception = assertThrows(ProfileNotFoundException.class, () -> facade.deleteById(id));
@@ -176,6 +186,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldNotDeleteProfileById_ProfileNotExists() {
+        doReturn(deleteCommand).when(applicationContext).getBean("profilePrincipalDelete", PrincipalProfileCommand.class);
         Long id = 603L;
         ProfileNotFoundException thrown = assertThrows(ProfileNotFoundException.class, () -> facade.deleteById(id));
 
@@ -189,6 +200,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldDeleteProfileInstance() throws ProfileNotFoundException {
+        doReturn(deleteCommand).when(applicationContext).getBean("profilePrincipalDelete", PrincipalProfileCommand.class);
         Long id = 614L;
         when(profile.getId()).thenReturn(id);
         doCallRealMethod().when(persistence).findPrincipalProfileById(id);
@@ -209,6 +221,7 @@ class PrincipalProfileFacadeImplTest {
 
     @Test
     void shouldNotDeleteProfileInstance_ProfileNotExists() throws ProfileNotFoundException {
+        doReturn(deleteCommand).when(applicationContext).getBean("profilePrincipalDelete", PrincipalProfileCommand.class);
         Long id = 716L;
         when(profile.getId()).thenReturn(id);
 
@@ -250,6 +263,9 @@ class PrincipalProfileFacadeImplTest {
         findCommand = spy(new FindPrincipalProfileCommand(persistence, payloadMapper));
         updateCommand = spy(new CreateOrUpdatePrincipalProfileCommand(persistence, payloadMapper));
         deleteCommand = spy(new DeletePrincipalProfileCommand(persistence, payloadMapper));
+        ReflectionTestUtils.setField(findCommand, "applicationContext", applicationContext);
+        ReflectionTestUtils.setField(updateCommand, "applicationContext", applicationContext);
+        ReflectionTestUtils.setField(deleteCommand, "applicationContext", applicationContext);
 
         return new PrincipalProfileCommandsFactory(
                 Set.of(findCommand, updateCommand, deleteCommand)
